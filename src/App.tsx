3a0615263d06d7566812a97d95d445acb47d6d85
EternalRival <eternalrival8@gmail.com>
2023-12-09 16:36:00 +0300

    feat: add temp gql app preview route

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,15 @@ import { AboutPage } from './pages/AboutPage';
 import { ERoutes } from './types/enums/ERoutes';
 import { UseLocalizationContext } from './context/LocalizationContext';
 import store from './store/store';
+import { GraphqlApp } from './components/GraphqlApp/GraphqlApp';
 
 const routes = createRoutesFromElements(
   <Route>
     <Route path={ERoutes.home} element={<MainPage />} />
     <Route path={ERoutes.about} element={<AboutPage />} />
+    <Route path="preview">
+      <Route path="gql" element={<GraphqlApp />} />
+    </Route>
   </Route>
 );
 
