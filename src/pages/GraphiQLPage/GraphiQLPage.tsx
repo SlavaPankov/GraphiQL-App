e7a2f6327e66173932056170719d3f13ca8e4347
EternalRival <eternalrival8@gmail.com>
2023-12-11 01:41:33 +0300

    refactor: move code crutch from facade

--- src/pages/GraphiQLPage/GraphiQLPage.tsx
+++ src/pages/GraphiQLPage/GraphiQLPage.tsx
@@ -5,28 +5,49 @@ import classNames from 'classnames';
 import styles from './graphiqlPage.module.scss'; // ? временно добавил styles.page до замены времянок на настоящие HEADER и FOOTER
 
 export function GraphiQLPage() {
+  /**
+   * Проблема:
+   * Невозможно обратиться к текущему значению State
+   * из-за типа `(data: Locale) => void`
+   *
+   * Решение:
+   * Замена типа `(data: Locale) => void` на `Dispatch<SetStateAction<Locale>>`
+   * (это изначальный тип функции `setLocale`, получаемый при использовании `useState`).
+   *
+   * Примечание:
+   * `ctx.locale` - это НЕ текущее значение State.
+   * `ctx.locale` - это значение снэпшота State на момент последнего рендера.
+   *
+   * https://react.dev/reference/react/useState#updating-state-based-on-the-previous-state
+   * */
   const { locale, setLocale } = useLocaleContext();
 
+  // ? временно добавил styles.page до замены времянок на настоящие HEADER и FOOTER
   return (
     <div className={classNames('container', styles.page)}>
-      {/* временно добавил styles.page до замены времянок на настоящие HEADER и FOOTER */}
-
       {/* TODO ⬇ заменить на настоящий HEADER */}
       <header>
+        <BaseButton label={`header: ${locale}`} disabled />
         <BaseButton
-          label={`header: ${locale}`}
+          label="en"
           onClick={() => {
-            setLocale((lang) => (lang === 'ru' ? 'en' : 'ru'));
+            setLocale('en');
+          }}
+        />
+        <BaseButton
+          label="ru"
+          onClick={() => {
+            setLocale('ru');
           }}
         />
       </header>
-      {/* TODO ⬆ заменить на настоящий HEADER */}
+
       <GraphqlApp />
+
       {/* TODO ⬇ заменить на настоящий FOOTER */}
       <footer>
         <BaseButton label="footer" />
       </footer>
-      {/* TODO ⬆ заменить на настоящий FOOTER */}
     </div>
   );
 }
