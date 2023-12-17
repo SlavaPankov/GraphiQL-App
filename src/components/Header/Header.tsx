4a9ef5783d033be98b93e6801e406acae1695e19
Gribanov_German <germangribanov@mail.ru>
2023-12-17 12:22:04 +0400

    refactor: change styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { localizationContext } from '@context/LocalizationContext';
 import { BaseButton } from '@components/BaseButton';
+import { useAppSelector } from '@hooks/useAppSelector';
 
 export function Header() {
   const { locale, setLocale, translate } = useContext(localizationContext);
@@ -9,12 +10,25 @@ export function Header() {
     setLocale(locale === 'ru' ? 'en' : 'ru');
   };
 
+  const handleSignOutClick = () => {
+    throw new Error('Put sign out function');
+  };
+
+  const isRegistered = useAppSelector(
+    (state) => state.api.config.middlewareRegistered
+  );
+
   return (
     <header>
       <nav>
         <Link to="/">{translate('To main')}</Link>
       </nav>
       <BaseButton label={translate('Language')} onClick={handleClick} />
+      {isRegistered && (
+        <button type="button" onClick={handleSignOutClick}>
+          {translate('Sign out')}
+        </button>
+      )}
     </header>
   );
 }
