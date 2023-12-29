4b0d5fe4cce603c8d9b88e47d53132e729cf3efe
EternalRival <eternalrival8@gmail.com>
2023-12-29 19:00:13 +0300

    feat: add parsed schema to docs explorer render

--- src/components/GraphqlApp/DocsExplorer/DocsExplorer.tsx
+++ src/components/GraphqlApp/DocsExplorer/DocsExplorer.tsx
@@ -3,22 +3,38 @@ import { Heading } from '@components/Heading';
 import { useLocaleContext } from '@context/LocalizationContext';
 import { useAppSelector } from '@hooks/useAppSelector';
 import classNames from 'classnames';
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, Suspense } from 'react';
+import { Await } from 'react-router-dom';
+import { parseSchema } from '@utils/gqlSchemaService';
 import styles from './docsExplorer.module.scss';
 
 export function DocsExplorer({
   className,
 }: Readonly<HTMLAttributes<HTMLElement>>) {
   const { translate } = useLocaleContext();
-  const data = useAppSelector((state) => state.graphqlQueryData.schema);
+  const schema = useAppSelector((state) => state.graphqlQueryData.schema);
+
+  const parsedSchema = parseSchema(schema);
   return (
     <Article
       className={classNames(className, styles.docsExplorerSection)}
       testId="docs-explorer"
     >
       <Heading>{translate('Docs')}</Heading>
-      <div>content</div>
-      <pre>{data}</pre>
+      <Suspense fallback="LOADING">
+        <Await resolve={parsedSchema}>
+          {({ queryType, types }: Awaited<typeof parsedSchema>) => (
+            <>
+              Root Types:
+              <div>query: {queryType.name}</div>
+              All Schema Types:
+              <pre>
+                {types.map(({ name, kind }) => `${name}: ${kind}`).join('\n')}
+              </pre>
+            </>
+          )}
+        </Await>
+      </Suspense>
     </Article>
   );
 }
